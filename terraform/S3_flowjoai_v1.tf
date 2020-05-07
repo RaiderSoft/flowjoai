51a91372db283c05ece567b9d13b9b9e86ee3edf
winee-design <57734227+winee-design@users.noreply.github.com>
2020-05-06 17:48:49 -0700

    added terraform file for s3 bucket

---
+++ terraform/S3_flowjoai_v1.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+    profile = "default"
+    region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "flowjoai-bucket" {
+  bucket = "flowjoai-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "flowjoai-v1"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_object" "outputs" {
+    bucket = "flowjoai-bucket"
+    acl    = "private"
+    key    = "outputs/"
+    source = "nul"
+    depends_on = [aws_s3_bucket.flowjoai-bucket]
+}
+
+resource "aws_s3_bucket_object" "training" {
+    bucket = "flowjoai-bucket"
+    acl    = "private"
+    key    = "training/"
+    source = "nul"
+    depends_on = [aws_s3_bucket.flowjoai-bucket]
+}
+
+resource "aws_s3_bucket_object" "v1_outputs" {
+    bucket = "flowjoai-bucket"
+    acl    = "private"
+    key    = "outputs/v1/"
+    source = "nul"
+    depends_on = [aws_s3_bucket.flowjoai-bucket]
+}
+
+resource "aws_s3_bucket_object" "v1_training" {
+    bucket = "flowjoai-bucket"
+    acl    = "private"
+    key    = "training/v1/"
+    source = "nul"
+    depends_on = [aws_s3_bucket.flowjoai-bucket]
+}
+
