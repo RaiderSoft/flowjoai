6c176878f5a17195cc0bd35f339c933a3543802d
Miguel Velazquez-palafox <miguelv@flowjo.com>
2020-05-09 12:25:31 -0700

    Fix lambda integrations

--- terraform/Websocket_flowjoai-websocket.tf
+++ terraform/Websocket_flowjoai-websocket.tf
@@ -13,11 +13,6 @@ resource "aws_apigatewayv2_api" "websocket" {
   route_selection_expression = "$request.body.action"
 }
 
-resource "aws_apigatewayv2_route" "connectRoute" {
-  api_id    = aws_apigatewayv2_api.websocket.id
-  route_key = "$connect"
-}
-
 resource "aws_apigatewayv2_route" "disconnectRoute" {
   api_id    = aws_apigatewayv2_api.websocket.id
   route_key = "$disconnect"
@@ -48,15 +43,19 @@ resource "aws_apigatewayv2_route" "default" {
   route_key = "$default"
 }
 
-resource "aws_apigatewayv2_integration" "connectRoute" {
-  api_id           = aws_apigatewayv2_api.websocket.id
-  integration_type = "AWS"
-
-
-  connection_type           = "INTERNET"
-  content_handling_strategy = "CONVERT_TO_TEXT"
-  description               = "function that runs upon client connection"
-  integration_method        = "POST"
-  integration_uri           = module.flowjoai_client_connect.connectFunction.invoke_arn
-  passthrough_behavior      = "WHEN_NO_MATCH"
+resource "aws_apigatewayv2_integration" "connectIntegration" {
+  api_id             = aws_apigatewayv2_api.websocket.id
+  integration_type   = "AWS_PROXY"
+  description        = "Connect Integration"
+  integration_uri    = module.flowjoai_client_connect.connectFunction.invoke_arn
+  integration_method = "POST"
 }
+
+resource "aws_apigatewayv2_route" "connectRoute" {
+  api_id         = aws_apigatewayv2_api.websocket.id
+  route_key      = "$connect"
+  operation_name = "connectRoute"
+  target         = "integrations/${aws_apigatewayv2_integration.connectIntegration.id}"
+
+}
+
