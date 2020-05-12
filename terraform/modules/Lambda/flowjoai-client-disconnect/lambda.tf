ec4d2999083952e689ee1c48cd36aefbdb8637d8
Miguel Velazquez-palafox <miguelv@flowjo.com>
2020-05-11 19:28:32 -0700

    update disconnect lambda

--- terraform/modules/Lambda/flowjoai-client-disconnect/lambda.tf
+++ terraform/modules/Lambda/flowjoai-client-disconnect/lambda.tf
@@ -1,21 +1,21 @@
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "disconnect_lambda_role"
-  assume_role_policy = file("../terraform/modules/Lambda/flowjoai-client-connect/iam/lambda_assume_policy.json")
+resource "aws_iam_role" "flowjoai-client-disconnect-iam-role" {
+  name = "flowjoai-client-disconnect-iam-role"
+  assume_role_policy = file("../terraform/modules/Lambda/flowjoai-client-disconnect/iam/lambda_assume_policy.json")
 }
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "disconnect_lambda_policy"
-  role = aws_iam_role.iam_for_lambda.id
-  policy = file("../terraform/modules/Lambda/flowjoai-client-connect/iam/lambda_policy.json")
+resource "aws_iam_role_policy" "flowjoai-client-disconnect-lambda-policy" {
+  name = "flowjoai-client-disconnect-lambda-policy"
+  role = aws_iam_role.flowjoai-client-disconnect-iam-role.id
+  policy = file("../terraform/modules/Lambda/flowjoai-client-disconnect/iam/lambda_policy.json")
 }
 
 resource "aws_lambda_function" "disconnectFunction" {
-  filename         = "../Lambda/flowjoai-client-disconnect/exports.js.zip"
+  filename         = "../terraform/modules/Lambda/flowjoai-client-disconnect/exports.js.zip"
   function_name    = "client-disconnect"
-  role             = aws_iam_role.iam_for_lambda.arn
+  role             = aws_iam_role.flowjoai-client-disconnect-iam-role.arn
   handler          = "exports.handler"
   runtime          = "nodejs12.x"
-  source_code_hash = filebase64sha256("../terraform/modules/Lambda/flowjoai-client-connect/exports.js.zip")
+  source_code_hash = filebase64sha256("../terraform/modules/Lambda/flowjoai-client-disconnect/exports.js.zip")
 }
 
 output "disconnectFunction" {
