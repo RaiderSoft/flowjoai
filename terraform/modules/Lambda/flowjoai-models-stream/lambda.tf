3d597c6fce8a67cbe8e7d1db7e86dcd389382056
Miguel Velazquez-palafox <miguelv@flowjo.com>
2020-05-12 15:22:55 -0700

    renames

--- terraform/modules/Lambda/flowjoai-models-stream/lambda.tf
+++ terraform/modules/Lambda/flowjoai-models-stream/lambda.tf
@@ -1,18 +1,18 @@
-resource "aws_iam_role" "flowjoai-client-disconnect-role" {
-  name = "flowjoai-client-disconnect-role"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "lambda_role"
   assume_role_policy = file("../terraform/modules/Lambda/flowjoai-client-connect/iam/lambda_assume_policy.json")
 }
 
-resource "aws_iam_role_policy" "flowjoai-client-disconnect-role-policy" {
-  name = "flowjoai-client-disconnect-role-policy"
-  role = aws_iam_role.flowjoai-client-disconnect-role.id
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "connect_lambda_policy"
+  role = aws_iam_role.iam_for_lambda.id
   policy = file("../terraform/modules/Lambda/flowjoai-client-connect/iam/lambda_policy.json")
 }
 
 resource "aws_lambda_function" "connectFunction" {
   filename         = "../terraform/modules/Lambda/flowjoai-client-connect/exports.js.zip"
   function_name    = "client-connect"
-  role             = aws_iam_role.flowjoai-client-disconnect-role.arn
+  role             = aws_iam_role.iam_for_lambda.arn
   handler          = "exports.handler"
   runtime          = "nodejs12.x"
   source_code_hash = filebase64sha256("../terraform/modules/Lambda/flowjoai-client-connect/exports.js.zip")
